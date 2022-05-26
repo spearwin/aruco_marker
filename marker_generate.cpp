52118ddbc56cbe77f02c45df4d4291ff8ddc9725
KCS <pon02124@unist.ac.kr>
2022-05-26 21:39:00 +0900

    tmp(0526)

--- marker_generate.cpp
+++ marker_generate.cpp
@@ -3,10 +3,22 @@
 #include <opencv2/highgui/highgui.hpp>
 
 int main() {
-    cv::Mat markerImage;
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-    cv::aruco::drawMarker(dictionary, 23, 200, markerImage, 1);
-    cv::imshow("marker", markerImage);
-    cv::waitKey();
-    // cv::imwrite("marker23.png", markerImage);
+    cv::Mat markerImage_1;
+    cv::Mat markerImage_2;
+    cv::Mat markerImage_3;
+    cv::Mat markerImage_4;
+
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+
+    cv::aruco::drawMarker(dictionary, 0, 300, markerImage_1, 1);
+    cv::aruco::drawMarker(dictionary, 1, 300, markerImage_2, 1);
+    cv::aruco::drawMarker(dictionary, 2, 300, markerImage_3, 1);
+    cv::aruco::drawMarker(dictionary, 3, 300, markerImage_4, 1);
+
+    // cv::imshow("marker", markerImage);
+    // cv::waitKey();
+    cv::imwrite("marker1.png", markerImage_1);
+    cv::imwrite("marker2.png", markerImage_2);
+    cv::imwrite("marker3.png", markerImage_3);
+    cv::imwrite("marker4.png", markerImage_4);
 }
